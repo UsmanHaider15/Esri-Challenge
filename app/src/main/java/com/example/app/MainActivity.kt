50f6af820f14fa1a9621ab3e6b9b6df7e31cbe57
Usman Haider <leo14haider@gmail.com>
2023-04-18 18:13:52 +0500

    add zoom control

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -7,14 +7,10 @@ import com.arcgismaps.ApiKey
 import com.arcgismaps.ArcGISEnvironment
 import com.arcgismaps.Color
 import com.arcgismaps.geometry.Point
-import com.arcgismaps.geometry.PolygonBuilder
-import com.arcgismaps.geometry.PolylineBuilder
 import com.arcgismaps.geometry.SpatialReference
 import com.arcgismaps.mapping.ArcGISMap
 import com.arcgismaps.mapping.BasemapStyle
 import com.arcgismaps.mapping.Viewpoint
-import com.arcgismaps.mapping.symbology.SimpleFillSymbol
-import com.arcgismaps.mapping.symbology.SimpleFillSymbolStyle
 import com.arcgismaps.mapping.symbology.SimpleLineSymbol
 import com.arcgismaps.mapping.symbology.SimpleLineSymbolStyle
 import com.arcgismaps.mapping.symbology.SimpleMarkerSymbol
@@ -24,7 +20,11 @@ import com.arcgismaps.mapping.view.GraphicsOverlay
 import com.arcgismaps.mapping.view.MapView
 import com.example.app.databinding.ActivityMainBinding
 import android.content.pm.PackageManager
-import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.zoom_control.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -57,6 +57,21 @@ class MainActivity : AppCompatActivity() {
         mapView.map = map
 
         mapView.setViewpoint(Viewpoint(1.293889, 103.793393, 72000.0))
+
+        // Set up zoom control
+        zoom_in_button.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val currentScale = mapView.mapScale.value / 2
+                mapView.setViewpointScale(currentScale)
+            }
+        }
+
+        zoom_out_button.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val currentScale = mapView.mapScale.value * 2
+                mapView.setViewpointScale(currentScale)
+            }
+        }
 
     }
 
@@ -92,11 +107,7 @@ class MainActivity : AppCompatActivity() {
         val point = Point(103.793393, 1.293889, SpatialReference.wgs84())
 
         // create a point symbol that is an small red circle
-        val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.red, 10f)
-
-        // create a blue outline symbol and assign it to the outline property of the simple marker symbol
-        val blueOutlineSymbol = SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.fromRgba(0, 0, 255), 2f)
-        simpleMarkerSymbol.outline = blueOutlineSymbol
+        val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.fromRgba(0, 0, 255), 10f)
 
         // create a graphic with the point geometry and symbol
         val pointGraphic = Graphic(point, simpleMarkerSymbol)
