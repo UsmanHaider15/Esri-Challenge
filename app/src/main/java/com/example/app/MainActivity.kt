35e8eb6ed6e7de2c35d381469a00f3f89392a08f
Usman Haider <leo14haider@gmail.com>
2023-04-18 15:47:26 +0500

    displaying map

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.arcgismaps.ApiKey
+import com.arcgismaps.ArcGISEnvironment
+import com.arcgismaps.mapping.ArcGISMap
+import com.arcgismaps.mapping.BasemapStyle
+import com.arcgismaps.mapping.Viewpoint
+import com.arcgismaps.mapping.view.MapView
+import com.example.app.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    private val mapView: MapView by lazy {
+        activityMainBinding.mapView
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        lifecycle.addObserver(mapView)
+
+        setApiKey()
+
+        setupMap()
+
+
     }
+
+    private fun setupMap() {
+
+        val map = ArcGISMap(BasemapStyle.ArcGISTopographic)
+
+        // set the map to be displayed in the layout's MapView
+        mapView.map = map
+
+        mapView.setViewpoint(Viewpoint(34.0270, -118.8050, 72000.0))
+
+    }
+
+    private fun setApiKey() {
+
+        // It is not best practice to store API keys in source code. We have you insert one here
+        // to streamline this tutorial.
+
+        ArcGISEnvironment.apiKey = ApiKey.create("You_API_KEY")
+
+    }
+
 }
